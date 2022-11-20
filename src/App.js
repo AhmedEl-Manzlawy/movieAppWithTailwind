faccc2c2a3c2afd9bc84978d7232ef846d560f65
ahmed elmanzlawy <ahmed.abdelnaser.elmanzlawy@gmail.com>
2022-11-20 14:10:04 +0200

    new push can be edit soon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar/Navbar"
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
+import KidsPage from './pages/KidsPage';
+import TheatresPage from './pages/TheatresPage';
+import DramaPage from './pages/DramaPage';
+import ComedyPage from './pages/ComedyPage';
 function App() {
   return ( 
     <div className=" min-h-screen">
@@ -11,6 +15,10 @@ function App() {
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path='/movie/:id' exact component={MoviePage} />
+          <Route path='/theatres' exact component={TheatresPage} />
+          <Route path='/kids' exact component={KidsPage} />
+          <Route path='/drama' exact component={DramaPage} />
+          <Route path='/comedy' exact component={ComedyPage} />
           <Route  path='*' exact component={ErrorPage} />
         </Switch>
       </Router>
